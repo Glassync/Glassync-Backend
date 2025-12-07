af0e3219320d08349ce43a7b114f8c305321a091
Ilya Kolomoitsev <i.kolomoitsev@yandex.ru>
2025-12-07 10:44:17 +0300

    feat: Added send notifications logic.

--- Glassync/management/commands/send_notification.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = 'Find users to need send notification.'
-
-    def handle(self, *args, **options):
-        # ToDo: Здесь должен быть код реализации отправки уведомлений пользователям.
-        return 0
