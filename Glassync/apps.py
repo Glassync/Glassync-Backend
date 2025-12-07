2a5cdc8b5bea911a9b6c15f1864638699671fa72
Ilya Kolomoitsev <i.kolomoitsev@yandex.ru>
2025-12-07 16:36:53 +0300

    feat: Added cron task initialization  for sending notifications.

--- Glassync/apps.py
+++ Glassync/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class GlassyncConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Glassync'
+
+    def ready(self):
+        call_command('init_cron_task')
